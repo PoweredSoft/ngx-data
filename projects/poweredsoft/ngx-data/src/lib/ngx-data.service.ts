99bcbf45ed8c9ec685ebe2eec33bec87d08c0eb7
David Lebee <david@poweredsoft.com>
2021-03-18 17:55:50 -0400

    before read in generic ds.

--- projects/poweredsoft/ngx-data/src/lib/ngx-data.service.ts
+++ projects/poweredsoft/ngx-data/src/lib/ngx-data.service.ts
@@ -3,8 +3,8 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IDataSourceTransportOptions, IDataSourceCommandAdapterOptions, IDataSourceOptions, IResolveCommandModelEvent, IDataSourceError, IDataSourceErrorMessage, IDataSourceValidationError } from '@poweredsoft/data';
 import { IQueryExecutionResult, IQueryExecutionGroupResult, IQueryCriteria } from '@poweredsoft/data';
 import { IDataSourceQueryAdapterOptions } from '@poweredsoft/data';
-import { catchError} from 'rxjs/operators';
-import { throwError, Observable } from 'rxjs';
+import { catchError, switchMap} from 'rxjs/operators';
+import { throwError, Observable, of } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -56,9 +56,9 @@ export class GenericRestDataSourceService
         });
     }
 
-    createDataSourceOptions<TModel, TKey>(route: string, keyResolver: (model: TModel) => TKey, defaultCriteria: IQueryCriteria, manageNotificationMessage: boolean = true) : IDataSourceOptions<TModel>
+    createDataSourceOptions<TModel, TKey>(route: string, keyResolver: (model: TModel) => TKey, defaultCriteria: IQueryCriteria, beforeRead?: (query: IQueryCriteria) => Observable<IQueryCriteria>) : IDataSourceOptions<TModel>
     {
-        const dataSourceTransportOptions = this.createStandardRestTransportOptions<TModel, TKey>(route, keyResolver);
+        const dataSourceTransportOptions = this.createStandardRestTransportOptions<TModel, TKey>(route, keyResolver, beforeRead);
 
         const dataSourceOptions: IDataSourceOptions<TModel> = {
             defaultCriteria: defaultCriteria,
@@ -73,13 +73,19 @@ export class GenericRestDataSourceService
         options.transport.commands[name].resolveCommandModel = resolveCommandModel;
     }
 
-    createStandardRestTransportOptions<TModel, TKey>(route: string, keyResolver: (model: TModel) => TKey) : IDataSourceTransportOptions<TModel> {
+    createStandardRestTransportOptions<TModel, TKey>(route: string, keyResolver: (model: TModel) => TKey, beforeRead?: (query: IQueryCriteria) => Observable<IQueryCriteria>) : IDataSourceTransportOptions<TModel> {
 
         const query: IDataSourceQueryAdapterOptions<TModel> = {
             adapter: {
                 handle: (criteria: IQueryCriteria) => {
                     const queryRoute = `${route}/read`;
-                    return this.http.post<IQueryExecutionResult<TModel> & IQueryExecutionGroupResult<TModel>>(queryRoute, criteria);
+                    const finalBeforeRead = beforeRead || (t => of(criteria));
+                    return finalBeforeRead(criteria)
+                        .pipe(
+                            switchMap(finalQuery => {
+                                return this.http.post<IQueryExecutionResult<TModel> & IQueryExecutionGroupResult<TModel>>(queryRoute, finalQuery);
+                            })
+                        );
                 }
             }
         };
